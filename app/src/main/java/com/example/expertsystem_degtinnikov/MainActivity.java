e073ff52e7b41a697aedb5f96cfc9621e9e23a5e
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-11-17 00:30:16 +0500

    Сделанная работа.Дегтянников.

--- app/src/main/java/com/example/expertsystem_degtinnikov/MainActivity.java
+++ app/src/main/java/com/example/expertsystem_degtinnikov/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.expertsystem_degtinnikov;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
@@ -15,8 +17,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
+    public Boolean selectRear;
+    public Boolean selectBiceps;
+    public Boolean selectTriceps;
+    public Boolean selectMuscles;
     public int nLayout = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,6 +82,10 @@ public class MainActivity extends AppCompatActivity {
                     || isTriceps.isChecked()
                     || isMuscles.isChecked())
             {
+                selectRear = isRear.isChecked();
+                selectBiceps = isBiceps.isChecked();
+                selectTriceps = isTriceps.isChecked();
+                selectMuscles = isMuscles.isChecked();
                 setContentView(R.layout.step_4);
                 nLayout = 4;
             }
@@ -115,6 +127,50 @@ public class MainActivity extends AppCompatActivity {
             else
             {
                 setContentView(R.layout.main);
+                if(!selectRear )
+                {
+                   findViewById(R.id.selectRear).setVisibility(View.GONE);
+                }
+                if(!selectBiceps )
+                {
+                    findViewById(R.id.selectBiceps).setVisibility(View.GONE);
+                }
+                if(!selectTriceps)
+                {
+                    findViewById(R.id.selectTriceps).setVisibility(View.GONE);
+                }
+                if(!selectMuscles)
+                {
+                    findViewById(R.id.selectMuscles).setVisibility(View.GONE);
+                }
+                findViewById(R.id.selectRear).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://rutube.ru/video/9baf44414453a59247ea6feeaf63c500/?r=wd"));
+                        startActivity(browserIntent);
+                    }
+                });
+                findViewById(R.id.selectBiceps).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://rutube.ru/video/8160f3bbf7ba03103a9ebb1874b989eb/?r=wd"));
+                        startActivity(browserIntent);
+                    }
+                });
+                findViewById(R.id.selectTriceps).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://rutube.ru/video/5c356b816db72f6d07a9349e2790875b/?r=wd"));
+                        startActivity(browserIntent);
+                    }
+                });
+                findViewById(R.id.selectMuscles).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://rutube.ru/video/68d4a66a4d3270dcc1c06ae4fc8bcf84/?r=wd"));
+                        startActivity(browserIntent);
+                    }
+                });
                 nLayout = 6;
             }
         }
